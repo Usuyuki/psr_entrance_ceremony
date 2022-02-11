3e024656dd2aacf76fe9fb5ef0711f4922797aa9
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-02-11 23:40:43 +0900

    fix 公開ディレクトリ修正、psr7表示確認

--- DocumentRoot/public/index.php
+++ DocumentRoot/public/index.php
@@ -1,2 +1,15 @@
 <?php
-echo "hello";
+require __DIR__ . '/../vendor/autoload.php';
+
+$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+$creator = new \Nyholm\Psr7Server\ServerRequestCreator(
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
+);
+
+$serverRequest = $creator->fromGlobals();
+
+var_dump($serverRequest->getUri());
